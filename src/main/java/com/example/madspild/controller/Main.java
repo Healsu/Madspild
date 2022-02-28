b5f7500636c89adc9309fbcf191235fb10f3f369
Healsu <timmie2500@gmail.com>
2022-02-28 10:21:03 +0100

    Initial commit

---
+++ src/main/java/com/example/madspild/controller/Main.java
@@ -0,0 +1,16 @@
+package com.example.madspild.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class Main {
+
+
+
+    @GetMapping("/Homepage")
+    public String homepage(){
+
+
+        return "Homepage";
+    }
+}
